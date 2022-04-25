3eb9f013029cdf5cab91031b08c0da698ede3e6d
RediG7 <guleksiredi7@gmail.com>
2022-04-25 21:11:25 +0200

    Filter Places Based on UserId

--- frontend/src/places/pages/UserPlaces.jsx
+++ frontend/src/places/pages/UserPlaces.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { useParams } from "react-router-dom";
 import PlaceList from "../components/PlaceList";
 
 const DUMMY_PLACES = [
@@ -30,7 +31,10 @@ const DUMMY_PLACES = [
 ];
 
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />;
+  // returns an object with dynamic segments (:userId) PROPERTIES
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
