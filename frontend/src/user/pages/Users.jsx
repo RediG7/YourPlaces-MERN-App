d078370c20491706ce303466a0ab1e3aa76d2b47
RediG7 <guleksiredi7@gmail.com>
2022-04-25 15:13:06 +0200

    Adding Testing Data

--- frontend/src/user/pages/Users.jsx
+++ frontend/src/user/pages/Users.jsx
@@ -1,5 +1,17 @@
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>User Works</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Redi Guleksi",
+      image:
+        "https://www.pexels.com/photo/man-and-woman-holding-vinyl-records-against-faces-7544626/",
+      places: 3,
+    },
+  ]; // Dummy constant
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
