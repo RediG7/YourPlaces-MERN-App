cd8b0158a283e0468bcac03af77ebf3bb44194de
RediG7 <guleksiredi7@gmail.com>
2022-04-25 18:14:21 +0200

    Import New Components & Add New Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,22 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="/places/new" element={<NewPlace />} />
-        {/* To keep the history clean, you should set replace prop. This will avoid extra redirects after the user click back. */}
-        <Route path="*" element={<Navigate to="/" replace />} /> 
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/:userId/places" element={<UserPlaces />} />
+          <Route path="/places/new" element={<NewPlace />} />
+          {/* To keep the history clean, you should set replace prop. This will avoid extra redirects after the user click back. */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 };
