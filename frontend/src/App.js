b8c27fb642a18969c25c5fef236128553472f0d4
RediG7 <guleksiredi7@gmail.com>
2022-05-01 13:04:29 +0200

    Update Place Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/:userId/places" element={<UserPlaces />} />
           <Route path="/places/new" element={<NewPlace />} />
           {/* To keep the history clean, you should set replace prop. This will avoid extra redirects after the user click back. */}
+          <Route path="/places/:placeId" element={<UpdatePlace />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
