dd3be76dab742442f11af2b41d0761fe6ac75ad3
RediG7 <guleksiredi7@gmail.com>
2022-05-01 13:03:07 +0200

    Config For React 18

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+// Update for React 18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
