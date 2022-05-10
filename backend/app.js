6564f28f8dee5a0c8816fcb3dc361441a0c318ac
RediG7 <guleksiredi7@gmail.com>
2022-05-10 20:39:13 +0200

    Replace duplicate Error Handling with Express Built-In Error Handling

--- backend/app.js
+++ backend/app.js
@@ -8,4 +8,19 @@ const app = express();
 // forwards to this route if it starts with /api/places
 app.use("/api/places", placesRoute); // => /api/places/... <= filter
 
+// Middleware function for error handling (express built-in)
+// recognized by express as a special middleware function aka error handling middleware function
+// executed on requests that have an error attached to it
+// this function will execute if any middleware in front of it yields an error
+app.use((error, req, res, next) => {
+  // Check if a response has already been sent
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
 app.listen(5000);
+
+// this function is applied on every incoming request -> app.use((req, res, next) => {});
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -20,9 +20,25 @@ const DUMMY_PLACES = [
 
 router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid; // { pid: 'p1'}
+  // find returns any or undefined
   const place = DUMMY_PLACES.find((p) => {
     return p.id === placeId;
   });
+
+  // place -> if not found it comes undefined here
+  // the opposite of undefined -> is true
+  /*  !undefined = true */
+  if (!place) {
+    // synchronous code: (can use throw error)
+    const error = new Error("Could not find a place with id: " + placeId);
+    error.code = 404; // dynamically adding property
+    throw error;
+
+    // old error handling way
+    // return res
+    //   .status(404)
+    //   .json({ message: "Place with id: " + placeId + "  Not Found" });
+  }
 
   // JS automatically expands this,
   // if the name of the property is equal to the name of the variable that holds the value you want to store in the property you can shorten it.
@@ -35,6 +51,20 @@ router.get("/user/:uid", (req, res, next) => {
   const place = DUMMY_PLACES.find((p) => {
     return p.creator === userId;
   });
+
+  if (!place) {
+    // asynchronous code: (must use next(error))
+    const error = new Error(
+      "Could not find a place for the provided user id: " + userId
+    );
+    error.code = 404; // dynamically adding property
+    return next(error);
+
+    // old error handling way
+    // return res.status(404).json({
+    //   message: `Could not find a place for the provided user id -> ${userId}`,
+    // });
+  }
 
   res.json({ place });
 });
