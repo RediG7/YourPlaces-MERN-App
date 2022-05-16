bdd70f23923f3dc6cb4ec54ac8f680fda7e8faf9
RediG7 <guleksiredi7@gmail.com>
2022-05-16 22:44:48 +0200

    Initial User Model

---
+++ backend/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  // unique: true -> creates an index for the email so it speeds up the query
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
+  places: { type: String, required: true },
+});
+
+// query as fast as possible, and make sure we can only create a new user if the email doesn't exist already
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
